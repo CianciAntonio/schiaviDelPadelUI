2d5f98594fbc548c3e38b8dfbc2bcc51c47d1ba1
CianciAntonio <antoniociancicico@gmail.com>
2023-10-26 09:40:09 +0200

    data binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,45 +6,53 @@ import { DatePipe } from '@angular/common';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   title = 'schiaviDelPadel';
 
   public currentDate = new Date();
-  public currentDateTimeString = this.currentDate.toLocaleString().replace(',','');
-  public currentDateMidNight = this.currentDate.toDateString();
+  public currentDateTimeString = this.currentDate
+    .toLocaleDateString()
+    .replace(',', '');
 
+  public daysOfWeek = []
   public slots?: SlotResponse[];
 
-  constructor(http: HttpClient){
-    http.get<SlotResponse[]>(`https://localhost:7077/Slot/ByDate?date=${this.currentDateTimeString}`).subscribe(
-      result=>{
+  constructor(http: HttpClient) {
+    http
+      .get<SlotResponse[]>(
+        `https://localhost:7077/Slot/ByDate?date=${this.currentDateTimeString}`
+      )
+      .subscribe((result) => {
         this.slots = result;
-      }
-    )
+        this.buildTimeSpans(this.slots);
+        console.log(this.currentDateTimeString)
+      });
   }
 
   timeSpans = [
-    {time : `${this.currentDateMidNight}`+' 08:30', user : ''},
-    {time : `${this.currentDateMidNight}`+' 10:00', user : ''},
-    {time : `${this.currentDateMidNight}`+' 11:30', user : ''},
-    {time : `${this.currentDateMidNight}`+' 13:00', user : ''},
-    {time : `${this.currentDateMidNight}`+' 14:30', user : ''},
-    {time : `${this.currentDateMidNight}`+' 16:00', user : ''},
-    {time : `${this.currentDateMidNight}`+' 17:30', user : ''},
-    {time : `${this.currentDateMidNight}`+' 19:00', user : ''},
-    {time : `${this.currentDateMidNight}`+' 20:30', user : ''},
-    {time : `${this.currentDateMidNight}`+' 22:00', user : ''}
-  ]
+    { time: `${this.currentDateTimeString}` + ' 08:30:00', user: '' },
+    { time: `${this.currentDateTimeString}` + ' 10:00:00', user: '' },
+    { time: `${this.currentDateTimeString}` + ' 11:30:00', user: '' },
+    { time: `${this.currentDateTimeString}` + ' 13:00:00', user: '' },
+    { time: `${this.currentDateTimeString}` + ' 14:30:00', user: '' },
+    { time: `${this.currentDateTimeString}` + ' 16:00:00', user: '' },
+    { time: `${this.currentDateTimeString}` + ' 17:30:00', user: '' },
+    { time: `${this.currentDateTimeString}` + ' 19:00:00', user: '' },
+    { time: `${this.currentDateTimeString}` + ' 20:30:00', user: '' },
+    { time: `${this.currentDateTimeString}` + ' 22:00:00', user: '' },
+  ];
 
-  ngOnInit(): void {    
-      // for(let timeSpan of this.timeSpans){
-      //   for(let slot of this.slots!){
-      //     if(timeSpan.time == slot.dateTime){
-      //       timeSpan.user = slot.userName
-      //     }
-      //   }
-      // }
+  buildTimeSpans(slots: any): void {
+    for (let timeSpan of this.timeSpans) {
+      for (let slot of slots) {
+        if (timeSpan.time == slot.dateTime) {
+          timeSpan.user = slot.userName;
+        }
+      }
+    }
+    console.log(this.timeSpans)
+    console.log(slots)
   }
 }
